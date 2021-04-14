0a26984bbb1bca48fbf156b281c667d7a2535f77
christian jamir santos piñeres <80717090+chrisiansanto@users.noreply.github.com>
2021-04-14 13:42:58 -0500

    Add files via upload

---
+++ Funciones_12DeAbril .py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+def resta(a, b):
+    return a - b
+resta(30, 10)
+
+
+# In[5]:
+
+
+def resta (a, b):
+    return a - b
+resta (b=30, a=10)
+
+
+# In[7]:
+
+
+m = [1, 4, 2, 7, 3]
+print(max(m))
+
+
+# In[9]:
+
+
+print(max(2, 3, 23))  
+
+
+# In[11]:
+
+
+list1 = [1, 2, 4, 5, 54] 
+print(max(list1)) # Devuelve 54 ya que 54 es el valor más grande de la lista
+
+list2 = ["a", "b", "c"]
+print(max(list2)) # 
+
+
+# In[1]:
+
+
+print(max(2, 3)) 
+
+
+# In[2]:
+
+
+q,r = divmod(13.5,2.5)  
+print(r) 
+print(q)
+
+
+# In[3]:
+
+
+list3 = [1, 2]
+print(min(list3)) 
+
+
+# In[ ]:
+
+
+
+
